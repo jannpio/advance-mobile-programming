54bff282ef8dc3e5604d096d7138cb693454c290
jannpio <julieannpioquinto11@gmail.com>
2025-09-14 22:49:26 +0800

    lab5

---
+++ pioquinto-back-end/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { getUsers, createUser, updateUser, deleteUser, loginUser, registerUser } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').put(updateUser).delete(deleteUser);
+
+router.post('/login', loginUser);
+
+router.post('/delete/:id', deleteUser);
+
+router.post('/register', registerUser);
+
+module.exports = router;
